f2abffc7ac2e7dc49e1672a74ab0652e3eb671af
Jachaplin <jakealanchaplin@yahoo.com>
2017-10-05 17:32:46 -0700

    Store artist input in firebase but repeats multiple times on page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,59 @@
+  var config = {
+    apiKey: "AIzaSyBXSp5la36OCI7Net3eh2U79Nc5UJfggCQ",
+    authDomain: "artist-360-app.firebaseapp.com",
+    databaseURL: "https://artist-360-app.firebaseio.com",
+    projectId: "artist-360-app",
+    storageBucket: "artist-360-app.appspot.com",
+    messagingSenderId: "1088102038579"
+  }
+  
+  firebase.initializeApp(config)
+
+  var database = firebase.database()
+    
     $("body").on("click","#artist-search-btn", function() {
       event.preventDefault()
       $(".bs-example-modal-lg").modal("hide")
       $("#content").empty()
+
+      // Initialize Firebase
+
+  //  Button for adding train
+
+  // $("body").on("click","#artist-search-btn", function(event) {
+  //   event.preventDefault()
+
+    // Grabs user input
+   
+    var artistName = $("#artist-name-input").val().trim()
+
+        // Creates local "temporary" object for holding train data
+
+    // var addArtist = {
+    //     name: artistName
+        
+    // }
+    // Uploads train data to the database
+    
+    database.ref().push(artistName)
+
+    // $("#artist-name-input").val("")
+
+    // Create Firebase event for adding trains to the database and a row in the html when a user adds an entry
+
+  database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+
+console.log(childSnapshot.val())
+
+    // Store everything into a variable.
+    
+    var artistName = childSnapshot.val()
+
+    $("#recent-search-table > tbody").append("<tr><td>" + artistName)
+
+  })
+
+  // })
 
 
     });
